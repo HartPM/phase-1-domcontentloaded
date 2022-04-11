156d4e8d7e70a237ee0a1cae28e3f770c5e38691
Patrick Hart <pathart612@gmail.com>
2022-04-11 12:45:52 -0400

    commit tree 237c9b77bb177f1d1c0998a9c4151b10462bbf10

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+const ptag = document.querySelector('#text');
+
+
+document.addEventListener('DOMContentLoaded', beLoaded);
+
+function beLoaded (e) {
+    return ptag.textContent = "This is really cool!"
+}
